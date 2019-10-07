51dde7ec7bba837522792c3da1bb09fa1897208f
djmac19 <daniel.mcentee@virgin.net>
2019-10-07 10:21:57 +0100

    hosting completed

--- db/migrations/20190916112030_create_comment_table.js
+++ db/migrations/20190916112030_create_comment_table.js
@@ -12,7 +12,7 @@ exports.up = function(knex) {
       .onDelete("CASCADE");
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
-    commentsTable.string("body").notNullable();
+    commentsTable.text("body").notNullable();
   });
 };
 
